45d912012946f384f0de75729680f35236c04fc3
Daniel Sommermann <dcsommer@gmail.com>
2011-12-11 14:37:19 -0800

    Debugged parsec based parser. Parsing should work again

--- src/Parser.hs
+++ src/Parser.hs
@@ -25,18 +25,18 @@ parseMGDS fname input = either (Left . show) (Right . id) $
 
 program :: Parser Program
 program = Program <$> go
-  where go  =  ((:) <$> function <*> go)
+  where go  =  P.try ((:) <$> function <*> go)
            <|> space *> P.eof *> (pure [])
 
 function :: Parser Function
-function = space *>  
+function = space *>
            (Function <$> name <*> (parseTuple name) <*
                          matchStr "=" <*> expression)
            <* space
 
 expression :: Parser Expression
-expression  =  ifParse 
-           <|> comp
+expression  =  P.try ifParse
+           <|> P.try comp
            <|> nested
 
 ifParse :: Parser Expression
@@ -48,33 +48,33 @@ functionCall :: Parser Expression
 functionCall = FunctionCall <$> name <*> (parseTuple expression)
 
 comp :: Parser Expression
-comp  =  binop Equals "==" logic
-     <|> binop Less "<" logic
-     <|> binop Greater ">" logic
+comp  =  P.try (binop Equals "==" logic)
+     <|> P.try (binop Less "<" logic)
+     <|> P.try (binop Greater ">" logic)
      <|> logic
 
 logic :: Parser Expression
-logic  =  Not <$> (matchStr "!" *> factor)
-      <|> binop LogicalAnd "&&" val
-      <|> binop LogicalOr "||" val
+logic  =  P.try (Not <$> (matchStr "!" *> factor))
+      <|> P.try (binop LogicalAnd "&&" val)
+      <|> P.try (binop LogicalOr "||" val)
       <|> val
 
 val :: Parser Expression
-val  =  binop Add "+" term
-    <|> binop Subtract "-" term
+val  =  P.try (binop Add "+" term)
+    <|> P.try (binop Subtract "-" term)
     <|> term
 
 term :: Parser Expression
-term  =  binop Multiply "*" factor
-     <|> binop Divide "/" factor
+term  =  P.try (binop Multiply "*" factor)
+     <|> P.try (binop Divide "/" factor)
      <|> factor
 
 factor :: Parser Expression
 factor  =  nested
-       <|> ifParse
-       <|> functionCall
-       <|> Constant <$> read <$> (many P.digit)
-       <|> Var <$> name
+       <|> P.try ifParse
+       <|> P.try functionCall
+       <|> P.try (Var <$> name)
+       <|> P.try (Constant <$> read <$> (many P.digit))
 
 nested :: Parser Expression
 nested = lparen *> expression <* rparen
@@ -87,19 +87,20 @@ name = (:) <$> P.letter <*> many P.alphaNum
 parseTuple :: Parser a -> Parser [a]
 parseTuple elemParse = 
   lparen *> list <* rparen
-  where list  =  (:) <$> elemParse <*> list'
-                 <|> pure [] -- in the case of an empty tuple
-        list' =  (:) <$> (comma *> elemParse) <*> list'
-                 <|> pure [] -- no more elements in the tuple
+  where list  =  P.try ((:) <$> elemParse <*> list')
+             <|> pure [] -- in the case of an empty tuple
+        list' =  P.try ((:) <$> (comma *> elemParse) <*> list')
+             <|> pure [] -- no more elements in the tuple
 
 binop ctor op nxt = ctor <$> nxt <* matchStr op <*> nxt
 line :: Parser Int
-line =  do p <- P.getPosition; return $ P.sourceLine p
+line =  do pos <- P.getPosition; return $ P.sourceLine pos
 col :: Parser Int
-col = do p <- P.getPosition; return $ P.sourceColumn p
+col = do pos <- P.getPosition; return $ P.sourceColumn pos
 
 --- Tokenizing
-space   =  (P.spaces *> comment *> space) <|> P.spaces
+space   =  P.try (P.spaces *> comment *> space)
+       <|> P.spaces
 
 matchStr op = space *> P.string op *> space
 comma   = matchStr ","
